7a2aacd54d42fc6066dc82126971335d6ad91326
AndreaCG1978 <44733992+AndreaCG1978@users.noreply.github.com>
2020-09-03 10:41:07 -0300

    Update SCRIPT-SHIPPING-LIST.sql

--- SCRIPT-SHIPPING-LIST.sql
+++ SCRIPT-SHIPPING-LIST.sql
@@ -77,7 +77,7 @@ CREATE TABLE `products` (
   `categoryId` int NOT NULL,
   `price` varchar(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 DROP TABLE IF EXISTS `products_orders`; 
 
